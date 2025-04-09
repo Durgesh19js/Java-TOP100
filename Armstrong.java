6db3a2e368ca79862afbf3db21264d6f850aa317
Durgesh19js <durgeshsonawane19@gmail.com>
2025-04-10 00:47:28 +0530

    Adding new files

---
+++ Armstrong.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Armstrong {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(isArmStrong(n));
+    }
+    static boolean isArmStrong(int n){
+        int original =n;
+        int sum =0;
+        while(n>0){
+            int rem=n%10;
+            n=n/10;
+            sum+=rem*rem*rem;
+
+        }
+        return sum==original;
+    }
+}
